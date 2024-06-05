12d617e6d6e8b10dad70231c524bba30fe5ca1bb
NoobToNinja <bondnuy007@me.com>
2024-06-05 14:02:27 +0700

    Update profiles.py

--- server/app/routers/profiles.py
+++ server/app/routers/profiles.py
@@ -1,20 +1,29 @@
 # app/routers/profile.py
 from fastapi import APIRouter, HTTPException
 from app.models.profile import Profile
-from app.database import get_db
+from app.database.connection import profiles_collection
+from app.schema.schemas import list_profile
+from bson import ObjectId
 
-router = APIRouter()
+profile_router = APIRouter()
 
-@router.get("/profiles/{profile_id}", response_model=Profile)
-async def get_profile(profile_id: str):
-    db = get_db()
-    profile = db.get_profile_by_id(profile_id)
+# @profile_router.get("/profiles/{profile_id}", response_model=Profile)
+# async def get_profile(profile_id: str):
+#     db = get_db()
+#     profile = db.get_profile_by_id(profile_id)
+#     if not profile:
+#         raise HTTPException(status_code=404, detail="Profile not found")
+#     return profile
+
+# @profile_router.post("/profiles/", response_model=Profile)
+# async def create_profile(profile: Profile):
+#     db = get_db()
+#     created_profile = db.create_profile(profile)
+#     return created_profile
+
+@profile_router.get("/profiles")
+async def get_address():
+    profile = list_profile(profiles_collection.find())
     if not profile:
-        raise HTTPException(status_code=404, detail="Profile not found")
+        raise HTTPException(status_code=404, detail="profiles not found")
     return profile
-
-@router.post("/profiles/", response_model=Profile)
-async def create_profile(profile: Profile):
-    db = get_db()
-    created_profile = db.create_profile(profile)
-    return created_profile
