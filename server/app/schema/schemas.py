ce74ac8c9cf8240245e8880b677370ddeb18e69c
NoobToNinja <bondnuy007@me.com>
2024-06-05 14:00:47 +0700

    Update schemas.py
    update code

--- server/app/schema/schemas.py
+++ server/app/schema/schemas.py
@@ -1,4 +1,6 @@
+
 from bson import ObjectId
+from pymongo.collection import Collection
 
 def individual_serial(todo) -> dict:
     return {
@@ -32,6 +34,7 @@ def user_serial(user) -> dict:
         "email": user["email"],
         "login_source": user["login_source"]
     }
+
 def list_user(users) -> list:
     return[user_serial(user) for user in users]
 
@@ -42,13 +45,13 @@ def list_user_id(users, user_id=None) -> list:
             return [user_serial(user)]
         else:
             return []
-    else:
+    else: 
         return [user_serial(user) for user in users]
 
 def address_serial(address) -> dict:
     return {
         "id": str(address["_id"]),
-        "user_id": address["user_id"],
+        "user_id": str(address["user_id"]),
         "street": address["street"],
         "city": address["city"],
         "state": address["state"],
@@ -57,16 +60,45 @@ def address_serial(address) -> dict:
     }
 
 def list_address(addresses) -> list:
-    return[user_serial(address) for address in addresses]
+    return[address_serial(address) for address in addresses]
 
 def profile_serial(profile) -> dict:
     return {
         "id": str(profile["_id"]),
-        "user_id": profile["user_id"],
-        "card_id": profile["current_address"],
+        "user_id": str(profile["user_id"]),
+        "card_id": profile["card_id"],
         "passport": profile["passport"],
         "phone_number": profile["phone_number"]
     }
 
 def list_profile(profiles) -> list:
-    return[user_serial(profiles) for profiles in profiles]
+    return[profile_serial(profile) for profile in profiles]
+
+# def list_user_id(users_collection, addresses_collection, profiles_collection, user_id=None) -> dict:
+#     try:
+#         object_id = ObjectId(user_id)
+#     except Exception as e:
+#         print(f"Error converting user_id to ObjectId: {e}")
+#         return None
+
+#     user = users_collection.find_one({"_id": object_id})
+#     if user:
+#         user_data = user_serial(user)
+#         address = addresses_collection.find_one({"user_id": object_id})
+#         profile = profiles_collection.find_one({"user_id": object_id})
+#         # Debugging statements to verify data
+#         print(f"user_id: {object_id}")
+#         print(f"Address found: {address}")
+#         print(f"Profile found: {profile}")
+#         if address:
+#             user_data["address"] = address_serial(address)
+#         else:
+#             user_data["address"] = None
+#         if profile:
+#             user_data["profile"] = profile_serial(profile)
+#         else:
+#             user_data["profile"] = None
+#         return user_data
+#     else:
+#         print(f"User not found with id: {user_id}")
+#         return None
