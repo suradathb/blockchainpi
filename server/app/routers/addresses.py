0732778b89a43d6be28e1992c4d490ef0ad9fc48
NoobToNinja <bondnuy007@me.com>
2024-06-05 14:02:02 +0700

    Update addresses.py

--- server/app/routers/addresses.py
+++ server/app/routers/addresses.py
@@ -1,20 +1,30 @@
 # app/routers/address.py
 from fastapi import APIRouter, HTTPException
 from app.models.address import Address
-from app.database import get_db
+from app.database.connection import addresses_collection
+from app.schema.schemas import list_address
+from bson import ObjectId
 
-router = APIRouter()
+router_address = APIRouter()
 
-@router.get("/addresses/{address_id}", response_model=Address)
-async def get_address(address_id: str):
-    db = get_db()
-    address = db.get_address_by_id(address_id)
+@router_address.get("/addresses")
+async def get_address():
+    address = list_address(addresses_collection.find())
     if not address:
         raise HTTPException(status_code=404, detail="Address not found")
     return address
 
-@router.post("/addresses/", response_model=Address)
-async def create_address(address: Address):
-    db = get_db()
-    created_address = db.create_address(address)
-    return created_address
+@router_address.get("/{user_id}")
+async def get_user_id(user_id: str):
+    address = addresses_collection.find_one({"user_id": user_id})
+    if address:
+        return list_address([address])
+    else:
+        return {"message": "address not found"}
+
+
+@router_address.post("/address/")
+async def add_address(address: Address):
+    address_dict = address.dict()
+    address_id = addresses_collection.insert_one(address_dict).inserted_id
+    return {"message": "Address added successfully", "address_id": str(address_id)}
